28ed44437e609dfca16e6423032f20d577d7e726
TMB <146067045+tmb101tech@users.noreply.github.com>
2025-09-16 00:29:49 +0100

    Deploy All Pages

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+        services: 'services.html',
+        contact: 'contact.html',
+        admin: 'admin.html'
+      }
+    }
+  }
+})
